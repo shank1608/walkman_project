a0170e3127130ed7ec76b656a2113c37107f92a0
Shashank Thakker <shashankthakker16@gmail.com>
2019-12-24 15:29:01 -0500

    created walkman class and called it in main file
    Signed-off-by: Shashank Thakker <shashankthakker16@gmail.com>

---
+++ main.py
@@ -0,0 +1,25 @@
+from walkman import Walkman
+
+
+def main():
+    songs= ["Mama", "its my life", "counting stars"]
+    playlist = Walkman(songs)
+    songs=playlist.convert_lower()
+    playlist.play("Mama")
+    playlist.next()
+    playlist.next()
+    playlist.next()
+    playlist.pause()
+    playlist.pause()
+    playlist.add_song(["Rose Tatto","All  fall down", "Morning"])
+    print (songs)
+    #playlist.remove_song("All  fall down")
+    #print (songs)
+    #playlist.remove_song("All  fall down")
+    #print (songs)
+
+
+
+
+if __name__  == '__main__':
+    main()
---
+++ walkman.py
@@ -0,0 +1,45 @@
+class Walkman():
+    def __init__(self,songs):
+        self.songs= [x.lower() for x in songs]
+        self.status = False # shows walkman status
+    def play(self,song):
+        if song.lower() in self.songs:
+            print ("playing  song ", song)
+            self.current_song= song.lower()
+            self.status = True
+        else:
+            print ("song is not in the list")
+    def next(self):
+        newplaylist = self.songs
+        current_index= newplaylist.index(self.current_song)
+        try:
+            next_index= current_index+1
+            next_song = newplaylist[next_index]
+            print("playing next song ..", next_song)
+            self.current_song= next_song
+        except IndexError:
+            next_index =0
+            next_song = newplaylist[next_index]
+            print("playing next song ..", next_song)
+            self.current_song= next_song
+
+    def pause(self):
+        if self.status:
+            print("pausing current song", self.current_song)
+            self.status =False
+        else:
+            print("Walkman is already paused")
+
+
+    def add_song(self,*songs):
+        return self.songs.extend(*songs)
+
+    def remove_song(self, song):
+        try:
+            return self.songs.remove(song.lower)
+        except ValueError:
+            print("song is not in the list")
+
+    def convert_lower(self):
+        #self.lower_song=map(lambda x:x.lower(),self.songs))
+        return lower_songs= [x.lower() for x in self.songs]
