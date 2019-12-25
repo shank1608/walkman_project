27a72d0c4f85ccf13da73a7af128182d3ac8898f
Ankur Saxena <ankur.saxena@corecompete.com>
2019-12-25 13:31:17 -0500

    song names made case insensitive

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ from walkman import Walkman
 
 
 def main():
-    songs= ["Mama", "its my life", "counting stars"]
+    songs = ["Mama", "its my life", "counting stars"]
     playlist = Walkman(songs)
-    songs=playlist.convert_lower()
     playlist.play("Mama")
     playlist.next()
     playlist.next()
     playlist.next()
     playlist.pause()
     playlist.pause()
-    playlist.add_song(("Rose Tatto","All  fall down", "Morning"))
-    print (songs)
+    playlist.add_song("Rose Tattoo", "All  fall down", "Morning")
+    print(playlist.songs)
     #playlist.remove_song("All  fall down")
     #print (songs)
     #playlist.remove_song("All  fall down")
     #print (songs)
-
-
 
 
 if __name__  == '__main__':
--- walkman.py
+++ walkman.py
@@ -1,45 +1,48 @@
 class Walkman():
-    def __init__(self,songs):
-        self.songs= [x.lower() for x in songs]
-        self.status = False # shows walkman status
-    def play(self,song):
-        if song.lower() in self.songs:
-            print ("playing  song ", song)
-            self.current_song= song.lower()
+    def __init__(self, songs):
+        self.songs = self.convert_lower(songs)
+        self.status = False  # shows walkman status
+
+    def play(self, song):
+        song_lower = song.lower()
+        if song_lower in self.songs:
+            print("playing  song ", song)
+            self.current_song = song_lower
             self.status = True
         else:
-            print ("song is not in the list")
+            print("song is not in the list")
+
     def next(self):
         newplaylist = self.songs
-        current_index= newplaylist.index(self.current_song)
+        current_index = newplaylist.index(self.current_song)
         try:
-            next_index= current_index+1
+            next_index = current_index + 1
             next_song = newplaylist[next_index]
             print("playing next song ..", next_song)
-            self.current_song= next_song
+            self.current_song = next_song
         except IndexError:
-            next_index =0
+            next_index = 0
             next_song = newplaylist[next_index]
             print("playing next song ..", next_song)
-            self.current_song= next_song
+            self.current_song = next_song
 
     def pause(self):
         if self.status:
             print("pausing current song", self.current_song)
-            self.status =False
+            self.status = False
         else:
             print("Walkman is already paused")
 
+    def add_song(self, *new_songs):
+        for song in new_songs:
+            self.songs.append(song.lower())
 
-    def add_song(self,*addedsongs):
-         self.songs.extend(*addedsongs)
-
-    def remove_song(self, song):
+    def remove_song(self, songs):
         try:
-             self.songs.remove(song.lower())
+            for song in songs:
+                self.songs.remove(song.lower())
         except ValueError:
             print("song is not in the list")
 
-    def convert_lower(self):
-        #self.lower_song=map(lambda x:x.lower(),self.songs))
-        return [x.lower() for x in self.songs]
+    def convert_lower(self, songs):
+        return [x.lower() for x in songs]
